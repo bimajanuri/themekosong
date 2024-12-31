2242527c64c6f0925bed002a63f44bbfb3a93dd1
bimajanuri <bimajanuri@gmail.com>
2024-12-31 13:51:47 +0700

    initial files

---
+++ footer.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+<footer>
+        <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// Thema kosong HeadlessWordPress
+get_header();
+?>
+<div id="main-content">
+    <h1>Area Terlarang</h1>
+    <p>Apabila anda melihat halaman ini, berarti anda telah memasuki area terlarang</p>
+</div>
+<?php
+get_footer();
+?>
