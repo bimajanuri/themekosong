23269482be5c16f7a363fa367b64c6516cc38661
bimajanuri <bimajanuri@gmail.com>
2025-01-22 09:48:19 +0700

    Add WP Navigation Menu

--- footer.php
+++ footer.php
@@ -1,6 +1,16 @@
 <html>
     <body>
         <footer>
+            <nav>
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'footer', // Lokasi menu yang didaftarkan di functions.php
+                    'menu_class' => 'footer-menu', // Kelas CSS untuk elemen menu
+                    'container' => 'ul', // Gunakan elemen <ul> sebagai wadah menu
+                    'fallback_cb' => false, // Jangan tampilkan fallback jika menu belum diset
+                ));
+                ?>
+            </nav>
             <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> | Semua Hak Cipta Dilindungi</p>
         </footer>
         <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -8,5 +8,15 @@
 </head>
 <body <?php body_class(); ?>>
     <header>
+        <nav>
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'primary', // Lokasi menu yang didaftarkan di functions.php
+                'menu_class' => 'primary-menu', // Kelas CSS untuk elemen menu
+                'container' => 'ul', // Gunakan elemen <ul> sebagai wadah menu
+                'fallback_cb' => false, // Jangan tampilkan fallback jika menu belum diset
+            ));
+            ?>
+        </nav>
     </header>
 </body>
