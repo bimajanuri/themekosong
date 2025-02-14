17e2ba7d26cd0968cbc27bf9a016cc6b263f411c
bimajanuri <bimajanuri@gmail.com>
2025-02-14 20:37:00 +0700

    add functions.php

--- functions.php
+++ functions.php
@@ -1,15 +1,14 @@
 <?php
-    function my_theme_setup()
-    {
-        // Menambahkan dukungan untuk menu
-        add_theme_support('menus');
-        add_theme_support('post-thumbnails');
+function my_theme_setup()
+{
+    // Menambahkan dukungan untuk menu
+    add_theme_support('menus');
+    add_theme_support('post-thumbnails');
 
-        // Mendaftarkan lokasi menu
-        register_nav_menus(array(
-            'primary' => __('Primary Menu', 'mytheme'),
-            'footer' => __('Footer Menu', 'mytheme'),
-        ));
-    }
-    add_action('after_setup_theme', 'my_theme_setup');
-?>
+    // Mendaftarkan lokasi menu
+    register_nav_menus(array(
+        'primary' => __('Primary Menu', 'mytheme'),
+        'footer' => __('Footer Menu', 'mytheme'),
+    ));
+}
+add_action('after_setup_theme', 'my_theme_setup');
